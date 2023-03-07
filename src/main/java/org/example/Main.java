4a3ffaa4be5fa8debb24760872e5c5fa443a4899
jokull <joj38@hi.is>
2023-03-07 11:52:22 +0000

    asldkjaælsd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        LibrarySystem librarySystem = new LibrarySystem();
+
+        librarySystem.addStudentUser("Ivanovich", true);
+
+
     }
 }
