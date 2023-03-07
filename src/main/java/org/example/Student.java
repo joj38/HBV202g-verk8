face8913f114b9223b6661e90b616f7cc0f63df5
BesturogMestur <birgirsig02@gmail.com>
2023-03-07 10:44:52 +0000

    user er tilbúð

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,4 +1,18 @@
 package org.example;
 
-public class Student {
+public class Student extends User{
+    private boolean feePiad;
+
+    public Student(String name, boolean feePiad){
+        super(name);
+        this.feePiad=feePiad;
+    }
+
+    public boolean isFeePiad(){
+        return feePiad;
+    }
+
+    public void setFeePiad(boolean feePiad) {
+        this.feePiad = feePiad;
+    }
 }
